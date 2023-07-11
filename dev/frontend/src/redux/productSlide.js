72770657e2c3651345504c65f5093f93d07f83d9
unknown <bkhdhoha@gmail.com>
2023-07-11 20:25:51 +0100

    first

---
+++ dev/frontend/src/redux/productSlide.js
@@ -0,0 +1,104 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-hot-toast";
+
+const initialState = {
+  productList: [],
+  cartItem: [],
+};
+
+export const productSlice = createSlice({
+  name: "product",
+  initialState,
+  reducers: {
+    setDataProduct: (state, action) => {
+      console.log(action);
+      // state.productList = [...action.payload];
+    },
+    
+   /* addCartItem: (state, action) => {
+      const check = state.cartItem.some((el) => el._id === action.payload._id);
+      if (check) {
+        toast("Already Item in Cart");
+      } else {
+        toast("Item Add successfully");
+        const total = action.payload.price;
+        state.cartItem = [
+          ...state.cartItem,
+          { ...action.payload, qty: 1, total: total },
+        ];
+      }
+    }, */
+    addCartItem: (state, action) => {
+      const { payload } = action;
+      if (!payload || !payload._id || typeof payload.price === "undefined") {
+        // Handle the case when payload, payload._id, or payload.price is undefined
+        return state;
+      }
+
+      const check = state.cartItem.some((el) => el._id === payload._id);
+      if (check) {
+        toast("Already Item in Cart");
+      } else {
+        toast("Item Add successfully");
+        const total = payload.price;
+        state.cartItem = [
+          ...state.cartItem,
+          { ...payload, qty: 1, total },
+        ];
+      }
+    },
+    deleteCartItem: (state, action) => {
+      toast("one Item Delete");
+      const index = state.cartItem.findIndex((el) => el._id === action);
+      state.cartItem.splice(index, 1);
+      console.log(index);
+    },
+    increaseQty: (state, action) => {
+      const index = state.cartItem.findIndex((el) => el._id === action.payload);
+      let qty = state.cartItem[index].qty;
+      const qtyInc = qty + 1;
+      state.cartItem[index].qty = qtyInc;
+
+      const price = state.cartItem[index].price;
+      const total = price * qtyInc;
+
+      state.cartItem[index].total = total;
+    },
+    decreaseQty: (state, action) => {
+      const index = state.cartItem.findIndex((el) => el._id === action.payload);
+      let qty = state.cartItem[index].qty;
+      if (qty > 1) {
+        const qtyDec = qty - 1;
+        state.cartItem[index].qty = qtyDec;
+
+        const price = state.cartItem[index].price;
+        const total = price * qtyDec;
+
+        state.cartItem[index].total = total;
+      }
+    },
+  },
+});
+
+
+export const {
+  setDataProduct,
+  addCartItem,
+  deleteCartItem,
+  increaseQty,
+  decreaseQty,
+} = productSlice.actions;
+
+export default productSlice.reducer;
+
+
+
+
+
+
+
+
+
+
+    
+   
